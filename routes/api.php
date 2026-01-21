f7cca3f6ac98d06b0f5084ccc0be435bdc78740a
Matias Vega <mvega@freelancerpy.com>
2026-01-21 11:07:31 -0300

    chore: register jsonapi v1 routes and install Laravel Sanctum

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Routing\ResourceRegistrar;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+JsonApiRoute::server('v1')
+    ->prefix('v1')
+    ->resources(function (ResourceRegistrar $server) {
+        // ...
+    });
